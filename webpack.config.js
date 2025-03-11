d3bc66fdcc8f63505f27c6a1d8a834ab0d647dd6
Valery-Ku <valeria.valera-valer@yandex.ru>
2025-03-11 20:37:56 +0000

    fonts+images

--- webpack.config.js
+++ webpack.config.js
@@ -7,38 +7,51 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, "build"),
-        assetModuleFilename: 'assets/[name][ext]',
         clean: true,
+        assetModuleFilename: 'assets/[name][ext]',
     },
- plugins: [new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, "src", "index.html"),
- }),
- new MiniCssExtractPlugin({
-    filename: "assets/style.css",
-})
-],
- module: {
-    rules: [
-        {
-            test: /\.html$/i,
-            loader: "html-loader",
-        },
-        {
-            test: /\.css$/i,
-            use: [MiniCssExtractPlugin.loader, "css-loader"],
-          },
-          {
-            test: /\.(?:js|mjs|cjs)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                targets: "defaults",
-                presets: [
-                  ['@babel/preset-env']
-                ]
-              }
-            }
-          },
-        ]
-}}
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html"),
+        }),
+        new MiniCssExtractPlugin({
+            filename: "assets/style.css",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    }
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "fonts/[name][ext]",
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "images/[name][ext]",
+                }
+            },
+        ],
+    },
+};

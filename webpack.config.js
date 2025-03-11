9e0f811481845d8b936f8409334d64471686c892
Valery-Ku <valeria.valera-valer@yandex.ru>
2025-03-11 19:33:18 +0000

    babel успешно

--- webpack.config.js
+++ webpack.config.js
@@ -26,5 +26,19 @@ module.exports = {
         {
             test: /\.css$/i,
             use: [MiniCssExtractPlugin.loader, "css-loader"],
-          },]
+          },
+          {
+            test: /\.(?:js|mjs|cjs)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                targets: "defaults",
+                presets: [
+                  ['@babel/preset-env']
+                ]
+              }
+            }
+          },
+        ]
 }}

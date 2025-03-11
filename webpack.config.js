538458e7ca6edc73a19deb493f3bea98131557b3
Valery-Ku <valeria.valera-valer@yandex.ru>
2025-03-11 15:10:51 +0000

    css установка

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path'); 
 
 module.exports = {
@@ -11,4 +12,11 @@ module.exports = {
     },
  plugins: [new HtmlWebpackPlugin({
     template: path.resolve(__dirname, "src", "index.html"),
- })]}
+ })],
+ module: {
+    rules: [
+        {
+            test: /\.html$/i,
+            loader: "html-loader",
+        }]
+}}

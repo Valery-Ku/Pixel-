6860bef72bbfd8a4b98869b8fd95aef99632ede7
Valery-Ku <valeria.valera-valer@yandex.ru>
2025-03-11 14:57:26 +0000

    html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path'); 
 
-module.exports = { 
-entry: "./src/index.js", 
-output: { 
-filename: 'bundle.js', 
-path: path.resolve(__dirname, "build"), 
-assetModuleFilename: 'assets/[name][ext]', 
-clean: true },
-};
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, "build"),
+        assetModuleFilename: 'assets/[name][ext]',
+        clean: true
+    },
+ plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, "src", "index.html"),
+ })]}
